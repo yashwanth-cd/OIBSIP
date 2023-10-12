7e3bfad6a5371f5b66d84332e5ad79879954756f
yashwanth-cd <143883595+yashwanth-cd@users.noreply.github.com>
2023-10-12 19:24:10 +0530

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,38 @@
+
+
+// project links
+
+const project1 = document.getElementById('project1');
+project1.addEventListener('click', function () {
+  window.open('https://yashwanth-cd.github.io/pig_game/', '_blank');
+});
+
+const project2 = document.getElementById('project2');
+project2.addEventListener('click', function () {
+  window.open('https://yashwanth-cd.github.io/guess_my_number/', '_blank');
+});
+
+const project3 = document.getElementById('project3');
+project3.addEventListener('click', function () {
+  window.open('https://yashwanth-cd.github.io/OIBSIP/Landing Page/', '_blank');
+});
+
+// Adding smooth scrolling to anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute('href').substring(1); // Remove the "#" from the href
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
+
+// Adding redirecting pages for contact details
+const linkedIn = document.getElementById('linkedin-link');
+linkedIn.addEventListener('click', function () {
+  window.open('https://www.linkedin.com/in/yashwanth-sai-c-5562851b2/', '_blank');
+});
